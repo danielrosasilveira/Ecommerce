d5ca09a54b83385db51bbaa6e2addecf075e6448
Daniel Rosa da Silveira <danielrosasilveira@gmail.com>
2020-01-31 16:10:06 -0300

    Model pages to project

--- Ecommerce/Ecommerce/Startup.cs
+++ Ecommerce/Ecommerce/Startup.cs
@@ -39,19 +39,24 @@ namespace Ecommerce
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
+            app.UseDefaultFiles();
             app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthorization();
-
+            
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            
+
         }
     }
 }
